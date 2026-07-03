34807ec6db7a0d2bd28cd655a07281bc57718917
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-07-03 07:24:03 -0900

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+import { connectDB } from "./lib/db.js";
+import cors from "cors"
+
+import dns from "node:dns/promises"
+dns.setServers([
+    '1.1.1.1',
+    '8.8.8.8'
+])
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors())
+
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
